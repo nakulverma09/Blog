1a590a66f032f1f40758a5b082e04c2411b580cb
Nakul Verma <workwithnakul09@gmail.com>
2025-04-09 21:39:00 +0530

    other changes save

--- backend/index.js
+++ backend/index.js
@@ -170,8 +170,9 @@ app.get("/refresh-token", (req, res) => {
 
     const user = { id: decoded.id, username: decoded.username }; // include any payload you use
     const newAccessToken = jwt.sign(user, process.env.ACCESS_SECRET, { expiresIn: "7d" });
+    const newRefreshToken = jwt.sign(user, process.env.REFRESH_SECRET, { expiresIn: "7d" });
 
-    res.json({ accessToken: newAccessToken, user }); // optional: return user info too
+    res.json({ accessToken: newAccessToken,newRefreshToken, user }); // optional: return user info too
   });
 });
 
