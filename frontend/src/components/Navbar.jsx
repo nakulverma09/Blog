7abbd23ef56e83b269ae6ff0219afe1bd7dc0f26
Nakul Verma <workwithnakul09@gmail.com>
2025-04-10 14:47:31 +0530

    some changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       console.log("Access Token:", token);
       if (!token) throw new Error("No token found");
 
-      const res = await axios.get("https://creative-86-backend.onrender.com/verify-token", {
+      const res = await axios.get("/verify-token", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -55,7 +55,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {  
     try {
-      await axios.post("https://creative-86-backend.onrender.com/logout", {}, {
+      await axios.post("/logout", {}, {
         withCredentials: true, // 👈 VERY IMPORTANT for cookies
       });
   
