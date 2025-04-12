2e57aaf41903103bafdd5f9bf0eeeed36bc9283f
Nakul Verma <workwithnakul09@gmail.com>
2025-04-12 15:30:35 +0530

    some changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/authController');
+const { signup } = require('../controllers/authControllers.js');
 
 // router.get('/', getUsers);
 router.post('/signup', signup);
