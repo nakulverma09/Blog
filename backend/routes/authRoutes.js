53ffac5411c0e7cef487732fe9a67fe9faa46f7f
Nakul Verma <workwithnakul09@gmail.com>
2025-04-12 15:23:21 +0530

    some changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,7 @@
-import express from 'express';
-
+const express = require('express');
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/signup', createUser);
 
-export default router;
+module.exports = router;
