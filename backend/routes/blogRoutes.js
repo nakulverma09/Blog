bbded16640b7f90a2c51a052101a6ab3a52ba098
Nakul Verma <workwithnakul09@gmail.com>
2025-04-13 16:26:03 +0530

    some bugs fixed in MVC

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -3,4 +3,5 @@ const authenticateToken = require("../middlewares/authMiddleware");
 const { blog } = require("../controllers/blogControllers.js"); // Import the blog controller
 const router = express.Router();
 
-router.post("/blog", authenticateToken, blog); // Route to handle blog creation')
+router.use(authenticateToken); // Apply authentication middleware to all routes in this file
+router.post("/blog", blog); // Route to handle blog creation')
--- backend/routes/tokenRoutes.js
+++ backend/routes/tokenRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { verifyToken, refreshToken } = require('../controllers/tokenControllers.js');
 const authenticateToken = require('../middlewares/authMiddleware.js');
 
-router.get('/verify-token', authenticateToken , verifyToken); // Handle GET requests to /token
+router.use(authenticateToken); // Apply authentication middleware to all routes in this file
+router.get('/verify-token', verifyToken); // Handle GET requests to /token
 router.get('/refresh-token', refreshToken); // Handle GET requests to /token/refresh
