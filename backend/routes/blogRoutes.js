3bbb240ffade37bee20c7988c3679596d81df4cf
Nakul Verma <workwithnakul09@gmail.com>
2025-04-13 16:16:25 +0530

    some bugs fixed in MVC

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authenticateToken = require("../middlewares/authMiddleware");
+const { blog } = require("../controllers/blogControllers.js"); // Import the blog controller
 const router = express.Router();
 
 router.post("/blog", authenticateToken, blog); // Route to handle blog creation')
