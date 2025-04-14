13667755dc8182fab4f3988a01974f9f31393e81
Nakul Verma <workwithnakul09@gmail.com>
2025-04-14 14:48:27 +0530

    some bugs fixed in MVC

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+  console.log("token is ", token)
   
   if (!token) return res.sendStatus(403); // <- this is what you're seeing
 
