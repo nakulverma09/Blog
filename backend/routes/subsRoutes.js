d2ece02fb9e992e6052360817d708b13a0ebd1bc
Nakul Verma <workwithnakul09@gmail.com>
2025-04-15 14:45:23 +0530

    some bugs fixed in MVC

--- backend/routes/subsRoutes.js
+++ backend/routes/subsRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { subscribe, contact } = require('../controllers/subsControllers.js');
+const { sendVerificationEmail } = require('../utils/mail.js'); // Import the email utility
 
 router.post('/subscribe', subscribe); // Handle POST requests to /subscribe
 router.post('/contact', contact); // Handle POST requests to /contact
 router.get("/test-email", async (req, res) => {
     try {
-      await sendVerificationEmail(process.env.EMAIL_USER, process.env.EMAIL_PASS);
+      await sendVerificationEmail("a04544868@gmail.com", "a04544868***");
       res.send("Email sent!");
     } catch (err) {
       console.error(err);
