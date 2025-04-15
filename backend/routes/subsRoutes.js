bfc85c442a30715ad682c1f99f98bdd3658cabfe
Nakul Verma <workwithnakul09@gmail.com>
2025-04-15 14:48:39 +0530

    some bugs fixed in MVC

--- backend/routes/subsRoutes.js
+++ backend/routes/subsRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { subscribe, contact } = require('../controllers/subsControllers.js');
-const { sendVerificationEmail } = require('../utils/mail.js'); // Import the email utility
+const sendVerificationEmail = require('../utils/mail.js'); // Import the email utility
 
 router.post('/subscribe', subscribe); // Handle POST requests to /subscribe
 router.post('/contact', contact); // Handle POST requests to /contact
