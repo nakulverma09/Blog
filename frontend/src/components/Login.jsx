bb1d68afbddd9f74edd4597532a3ce39b21427ca
Nakul Verma <workwithnakul09@gmail.com>
2025-04-16 12:25:31 +0530

    some bugs fixed in MVC

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, useSearchParams } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useContext } from "react";
 import { AppContext } from "../context/context";
