d7708068f6f6d84f61069394edb6bbeb20e9fa30
Nakul Verma <workwithnakul09@gmail.com>
2025-04-19 14:19:08 +0530

    some changes occur

--- backend/controllers/categoryControllers.js
+++ backend/controllers/categoryControllers.js
@@ -15,7 +15,7 @@ exports.educationAndLearning = async (req, res) => {
     const data = await Blog.find({ category: "Education & Learning" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching education and learning data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -25,7 +25,7 @@ exports.lifestyle = async (req, res) => {
     const data = await Blog.find({ category: "Lifestyle" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching lifestyle data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -35,7 +35,7 @@ exports.finance = async (req, res) => {
     const data = await Blog.find({ category: "Finance" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching finance data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -45,7 +45,7 @@ exports.entertainment = async (req, res) => {
     const data = await Blog.find({ category: "Entertainment" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching entertainment data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -55,7 +55,7 @@ exports.newsAndCurrentAffairs = async (req, res) => {
     const data = await Blog.find({ category: "News & Current Affairs" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching news and current affairs data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -65,7 +65,7 @@ exports.inspirationAndPersonalDevelopment = async (req, res) => {
     const data = await Blog.find({ category: "Inspiration & Personal Development" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching inspiration and personal development data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -75,7 +75,7 @@ exports.cultureAndHistory = async (req, res) => {
     const data = await Blog.find({ category: "Culture & History" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching culture and history data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -85,7 +85,7 @@ exports.codingAndProjects = async (req, res) => {
     const data = await Blog.find({ category: "Coding & Projects" }).populate("user", "username");
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching coding and projects data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -95,7 +95,7 @@ exports.miscellaneous = async (req, res) => {
     const data = await Blog.find({ category: "Miscellaneous" }).populate("user", "username"); // Populate user details
     res.status(200).json({ message: "Data fetch of technology successfully", data });
   } catch (error) {
-    console.error("Error fetching technology data:", error);
+    console.error("Error fetching miscelleaneous data:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
